a6b9eed6c75406aa894272b8cdf577a06b1a8fa6
David Mallard <correspondence@davidmallard.id.au>
2017-05-25 11:58:01 +1000

    Complete the Ice Cream quiz

--- 3-javascript-foundations/03-conditionals/13-ice-cream.js
+++ 3-javascript-foundations/03-conditionals/13-ice-cream.js
@@ -16,4 +16,6 @@ var flavor = "strawberry";
 var vessel = "cone";
 var toppings = "cookies";
 
-// Add your code here
+if ((flavor === 'vanilla' || flavor === 'chocolate') && (vessel === 'cone' || vessel === 'bowl') && (toppings === 'sprinkles' || toppings === 'peanuts')) {
+	console.log('I\'d like two scoops of ' + flavor + ' ice cream in a ' + vessel + ' with ' + toppings + '.');
+}
