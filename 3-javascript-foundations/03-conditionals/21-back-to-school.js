eafeebb48749178e3a2ca77dc714268ac8d52f24
David Mallard <correspondence@davidmallard.id.au>
2017-05-25 12:25:50 +1000

    Add the initial file for the Back To School quiz

---
+++ 3-javascript-foundations/03-conditionals/21-back-to-school.js
@@ -0,0 +1,11 @@
+/*
+ * Programming Quiz: Back to School (3-9)
+ */
+
+// change the value of `education` to test your code
+var education = "no high school diploma";
+
+// set the value of this based on a person's education
+var salary;
+
+// your code goes here
