ea4ca5766dee903b578a2f15cef109e799876bbc
David Mallard <correspondence@davidmallard.id.au>
2017-05-25 15:38:46 +1000

    Complete the Laugh It Off 1 quiz

--- 3-javascript-foundations/05-functions/05-laugh-it-off-1.js
+++ 3-javascript-foundations/05-functions/05-laugh-it-off-1.js
@@ -2,6 +2,8 @@
  * Programming Quiz: Laugh it Off 1 (5-1)
  */
 
-// your code goes here
+function laugh() {
+	return 'hahahahahahahahahaha!';
+}
 
-console.log(/* call the laugh function */);
+console.log(laugh());
