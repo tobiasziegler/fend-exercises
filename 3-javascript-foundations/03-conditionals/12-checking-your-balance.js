3cb76a8681bddbe8543d7b1de7c9b174c7e5fd93
David Mallard <correspondence@davidmallard.id.au>
2017-05-25 11:52:46 +1000

    Complete the Checking Your Balance quiz

--- 3-javascript-foundations/03-conditionals/12-checking-your-balance.js
+++ 3-javascript-foundations/03-conditionals/12-checking-your-balance.js
@@ -7,4 +7,14 @@ var balance = 325.00;
 var checkBalance = true;
 var isActive = false;
 
-// your code goes here
+if (!checkBalance) {
+	console.log('Thank you. Have a nice day!');
+} else if (isActive && balance > 0) {
+	console.log('Your balance is $' + balance.toFixed(2) +'.');
+} else if (!isActive) {
+	console.log('Your account is no longer active.');
+} else if (balance === 0) {
+	console.log('Your account is empty.');
+} else {
+	console.log('Your balance is negative. Please contact bank.');
+}
