0e36efded9a78f28a3c1ae44b8c149efd4d09be7
David Mallard <correspondence@davidmallard.id.au>
2017-05-25 15:43:00 +1000

    Complete the Laugh It Off 2 quiz

--- 3-javascript-foundations/05-functions/06-laugh-it-off-2.js
+++ 3-javascript-foundations/05-functions/06-laugh-it-off-2.js
@@ -2,6 +2,13 @@
  * Programming Quiz: Laugh it Off 2 (5-2)
  */
 
-// declare your function
-// then, call it!
-// print the output with console.log
+function laugh(num) {
+	var output = '';
+	for (var i = 0; i < num; i++) {
+		output += 'ha';
+	}
+	output += '!';
+	return output;
+}
+
+console.log(laugh(3));
