4a35c4dfd0cb69810a1ddc2b9fe0c3d43fd59d0f
David Mallard <correspondence@davidmallard.id.au>
2017-05-25 11:59:38 +1000

    Add the initial file for the What Do I Wear? quiz

---
+++ 3-javascript-foundations/03-conditionals/14-what-do-i-wear.js
@@ -0,0 +1,10 @@
+/*
+ * Programming Quiz: What do I Wear? (3-7)
+ */
+
+// change the values of `shirtWidth`, `shirtLength`, and `shirtSleeve` to test your code
+var shirtWidth = 23;
+var shirtLength = 30;
+var shirtSleeve = 8.71;
+
+// your code goes here
