68dcfaa47e6fac1bafccace40fca63ae171c5f09
David Mallard <correspondence@davidmallard.id.au>
2017-05-25 11:32:57 +1000

    Complete the Murder Mystery quiz

--- 3-javascript-foundations/03-conditionals/08-murder-mystery.js
+++ 3-javascript-foundations/03-conditionals/08-murder-mystery.js
@@ -9,16 +9,28 @@ var suspect = "Mr. Parkes";
 var weapon = "";
 var solved = false;
 
-if (/* your conditional goes here */) {
-    
-} else if (/* your conditional goes here */) {
-
-} else if (/* your conditional goes here */) {
-
-} else {
-
+if (room === 'ballroom') {
+    weapon = 'poison';
+	if (suspect === 'Mr. Kalehoff') {
+		solved = true;
+	}
+} else if (room === 'gallery') {
+	weapon = 'trophy';
+	if (suspect === 'Ms. Van Cleve') {
+		solved = true;
+	}
+} else if (room === 'billiards room') {
+	weapon = 'pool stick';
+	if (suspect === 'Mrs. Sparr') {
+		solved = true;
+	}
+} else { // dining room
+	weapon = 'knife';
+	if (suspect === 'Mr. Parkes') {
+		solved = true;
+	}
 }
 
 if (solved) {
-	console.log(/* your message goes here*/);
+	console.log(suspect + ' did it in the ' + room + ' with the ' + weapon + '!');
 }
