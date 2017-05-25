b4fc7a28cedf6e220a4419446606271c366c2bed
David Mallard <correspondence@davidmallard.id.au>
2017-05-25 11:53:44 +1000

    Add the initial file for the Ice Cream quiz

---
+++ 3-javascript-foundations/03-conditionals/13-ice-cream.js
@@ -0,0 +1,19 @@
+/*
+ * Programming Quiz: Ice Cream (3-6)
+ *
+ * Write a single if statement that logs out the message:
+ *
+ * "I'd like two scoops of __________ ice cream in a __________ with __________."
+ *
+ * ...only if:
+ *   - flavor is "vanilla" or "chocolate"
+ *   - vessel is "cone" or "bowl"
+ *   - toppings is "sprinkles" or "peanuts"
+ */
+
+// change the values of `flavor`, `vessel`, and `toppings` to test your code
+var flavor = "strawberry";
+var vessel = "cone";
+var toppings = "cookies";
+
+// Add your code here
