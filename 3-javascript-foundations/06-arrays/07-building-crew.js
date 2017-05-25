6a6cfae6de8795135036f1f1a54287a51574d74d
David Mallard <correspondence@davidmallard.id.au>
2017-05-25 17:54:41 +1000

    Complete the Building The Crew quiz

--- 3-javascript-foundations/06-arrays/07-building-crew.js
+++ 3-javascript-foundations/06-arrays/07-building-crew.js
@@ -9,4 +9,5 @@ var companion = "Inara";
 var mercenary = "Jayne";
 var mechanic = "Kaylee";
 
-// your code goes here
+var crew = [captain, second, pilot, companion, mercenary, mechanic];
+console.log(crew);
