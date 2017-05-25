7092172bc20356b17f9d26179fd04b6e195b8452
David Mallard <correspondence@davidmallard.id.au>
2017-05-25 11:19:16 +1000

    Add the initial file for the Murder Mystery quiz

---
+++ 3-javascript-foundations/03-conditionals/08-murder-mystery.js
@@ -0,0 +1,24 @@
+/*
+ * Programming Quiz: Murder Mystery (3-4)
+ */
+
+// change the value of `room` and `suspect` to test your code
+var room = "dining room";
+var suspect = "Mr. Parkes";
+
+var weapon = "";
+var solved = false;
+
+if (/* your conditional goes here */) {
+    
+} else if (/* your conditional goes here */) {
+
+} else if (/* your conditional goes here */) {
+
+} else {
+
+}
+
+if (solved) {
+	console.log(/* your message goes here*/);
+}
