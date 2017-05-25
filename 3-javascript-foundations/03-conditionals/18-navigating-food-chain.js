88cc789badb460323f1497757c91362406704f8d
David Mallard <correspondence@davidmallard.id.au>
2017-05-25 12:17:32 +1000

    Complete the Navigating The Food Chain quiz

--- 3-javascript-foundations/03-conditionals/18-navigating-food-chain.js
+++ 3-javascript-foundations/03-conditionals/18-navigating-food-chain.js
@@ -16,6 +16,6 @@
 var eatsPlants = false;
 var eatsAnimals = true;
 
-var category = /* your code goes here */
+var category = eatsPlants ? eatsAnimals ? 'omnivore' : 'herbivore' : eatsAnimals ? 'carnivore' : undefined;
 
 console.log(category);
