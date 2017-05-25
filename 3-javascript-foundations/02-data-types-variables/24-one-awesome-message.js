f638ac1259d440c413a416d4ba4952f479402b01
David Mallard <correspondence@davidmallard.id.au>
2017-05-25 10:52:48 +1000

    Add the initial file for the One Awesome message quiz

---
+++ 3-javascript-foundations/02-data-types-variables/24-one-awesome-message.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: One Awesome Message (2-12)
+ *
+ * 1. Create the variables
+ *     - firstName
+ *     - interest
+ *     - hobby
+ * 2. Create a variable named awesomeMessage and, using string concatenation and the variables above, create an awesome message.
+ * 3. Print the awesomeMessage variable to the console
+ */
+
+// Add your code here
