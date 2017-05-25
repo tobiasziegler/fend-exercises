589e2f35429be0bf90574f3981158268c73e7bcb
David Mallard <correspondence@davidmallard.id.au>
2017-05-25 18:12:00 +1000

    Add the initial file for the Quidditch Cup quiz

---
+++ 3-javascript-foundations/06-arrays/15-quidditch-cup.js
@@ -0,0 +1,8 @@
+/*
+ * Programming Quiz: Quidditch Cup (6-5)
+ */
+
+// your code goes here
+
+var team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", "Alicia Spinnet", "George Weasley", "Fred Weasley", "Harry Potter"];
+console.log(hasEnoughPlayers(team));
