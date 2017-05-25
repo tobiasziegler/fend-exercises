f869bd162b6df48cf9aab524a82cb19197e1c3a6
David Mallard <correspondence@davidmallard.id.au>
2017-05-25 11:41:39 +1000

    Add the initial file for the Checking Your Balance quiz

---
+++ 3-javascript-foundations/03-conditionals/12-checking-your-balance.js
@@ -0,0 +1,10 @@
+/*
+ * Programming Quiz - Checking Your Balance (3-5)
+ */
+
+// change the values of `balance`, `checkBalance`, and `isActive` to test your code
+var balance = 325.00;
+var checkBalance = true;
+var isActive = false;
+
+// your code goes here
