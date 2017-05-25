25b37e17306506b6a0c38d3ec3f1643578e54c71
David Mallard <correspondence@davidmallard.id.au>
2017-05-25 18:17:13 +1000

    Complete the Quidditch Cup quiz

--- 3-javascript-foundations/06-arrays/15-quidditch-cup.js
+++ 3-javascript-foundations/06-arrays/15-quidditch-cup.js
@@ -2,7 +2,13 @@
  * Programming Quiz: Quidditch Cup (6-5)
  */
 
-// your code goes here
+function hasEnoughPlayers(players) {
+	if (players.length >= 7) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
 var team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", "Alicia Spinnet", "George Weasley", "Fred Weasley", "Harry Potter"];
 console.log(hasEnoughPlayers(team));
