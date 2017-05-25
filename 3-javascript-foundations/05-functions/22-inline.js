9833205ba98bcd5519b790dc81144b9f709dc91d
David Mallard <correspondence@davidmallard.id.au>
2017-05-25 16:26:30 +1000

    Add the initial file for the Inline quiz

---
+++ 3-javascript-foundations/05-functions/22-inline.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: Inline Functions (5-6)
+ */
+
+// don't change this code
+function emotions(myString, myFunc) {
+    console.log("I am " + myString + ", " + myFunc(2));
+}
+
+// your code goes here
+// call the emotions function here and pass in an
+// inline function expression
