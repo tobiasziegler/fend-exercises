07b6d655b9736d9757e996680fe52b5ec6b950ab
David Mallard <correspondence@davidmallard.id.au>
2017-05-25 18:53:20 +1000

    Complete the I Got Bills quiz

--- 3-javascript-foundations/06-arrays/22-i-got-bills.js
+++ 3-javascript-foundations/06-arrays/22-i-got-bills.js
@@ -5,4 +5,8 @@
 var bills = [50.23, 19.12, 34.01, 100.11, 12.15, 9.90,
 29.11, 12.99, 10.00, 99.22, 102.20, 100.10, 6.77, 2.22];
 
-// your code goes here
+var totals = bills.map(function(bill) {
+	return Number((bill*1.15).toFixed(2));
+});
+
+console.log(totals);
