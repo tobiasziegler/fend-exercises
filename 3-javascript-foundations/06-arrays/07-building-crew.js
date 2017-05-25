e1f6cb76c1cfaf308a647db6e8f484b9fbd3b86c
David Mallard <correspondence@davidmallard.id.au>
2017-05-25 17:53:30 +1000

    Add the initial file for the Building The Crew quiz

---
+++ 3-javascript-foundations/06-arrays/07-building-crew.js
@@ -0,0 +1,12 @@
+/*
+ * Programming Quiz: Building the Crew (6-2)
+ */
+
+var captain = "Mal";
+var second = "Zoe";
+var pilot = "Wash";
+var companion = "Inara";
+var mercenary = "Jayne";
+var mechanic = "Kaylee";
+
+// your code goes here
