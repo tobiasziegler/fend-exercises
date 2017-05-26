5035ff907bd34cf44ee33a28308cb026028d34b9
David Mallard <correspondence@davidmallard.id.au>
2017-05-26 11:09:35 +1000

    Complete the Umbrella quiz

--- 3-javascript-foundations/07-objects/03-umbrella.js
+++ 3-javascript-foundations/07-objects/03-umbrella.js
@@ -13,5 +13,12 @@ var umbrella = {
             return "Julia opens the umbrella!";
         }
     },
-    // your code goes here
+    close: function() {
+		if (umbrella.isOpen === false) {
+			return 'The umbrella is already closed!';
+		} else {
+			umbrella.isOpen = false;
+			return 'Julia closes the umbrella!';
+		}
+	}
 };
