f9c8f80f10cab98581093682e62bbcf32ee5ce8e
David Mallard <correspondence@davidmallard.id.au>
2017-05-26 11:38:09 +1000

    Complete the Facebook Friends quiz

--- 3-javascript-foundations/07-objects/11-facebook-friends.js
+++ 3-javascript-foundations/07-objects/11-facebook-friends.js
@@ -2,4 +2,20 @@
  * Programming Quiz: Facebook Friends (7-5)
  */
 
-// your code goes here
+var facebookProfile = {
+	name: "David Mallard",
+	friends: 0,
+	messages: ['g\'day', 'yo', 'hi'],
+	postMessage: function(message) {
+		facebookProfile.messages.push(message);
+	},
+	deleteMessage: function(index) {
+		facebookProfile.messages.splice(index, 1);
+	},
+	addFriend: function() {
+		++facebookProfile.friends;
+	},
+	removeFriend: function() {
+		--facebookProfile.friends;
+	}
+};
