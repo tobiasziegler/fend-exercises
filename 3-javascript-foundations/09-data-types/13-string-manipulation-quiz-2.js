0d86425387422d0b1c202847b2cf5e6c56ae4217
David Mallard <correspondence@davidmallard.id.au>
2017-05-27 11:43:14 +1000

    Add the initial file for String Manipulation Quiz 2

---
+++ 3-javascript-foundations/09-data-types/13-string-manipulation-quiz-2.js
@@ -0,0 +1,14 @@
+var name = "AlbERt EINstEiN";
+
+function nameChanger(oldName) {
+    var finalName = oldName;
+    // Your code goes here!
+
+
+
+    // Don't delete this line!
+    return finalName;
+};
+
+// Did your code work? The line below will tell you!
+console.log(nameChanger(name));
