10df94fb6be560b39b1f67b9cd546d204b3cfda8
David Mallard <correspondence@davidmallard.id.au>
2017-05-27 11:06:31 +1000

    Complete the string.replace() quiz

--- 3-javascript-foundations/frontend-nanodegree-resume/js/resumeBuilder.js
+++ 3-javascript-foundations/frontend-nanodegree-resume/js/resumeBuilder.js
@@ -2,5 +2,10 @@
 // $("#main").append("Tobby");
 
 // Save and .append() Data
+// var awesomeThoughts = 'I am Tobby and I am AWESOME!';
+// console.log(awesomeThoughts);
+
+// string.replace()
 var awesomeThoughts = 'I am Tobby and I am AWESOME!';
-console.log(awesomeThoughts);
+funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+console.log(funThoughts);
