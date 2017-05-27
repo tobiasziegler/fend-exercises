697deba1c65debdcaa9f2f5ae67b69b9bf7b0d7e
David Mallard <correspondence@davidmallard.id.au>
2017-05-27 11:41:42 +1000

    Complete the Array Manipulation quiz

--- 3-javascript-foundations/09-data-types/12-array-manipulation.js
+++ 3-javascript-foundations/09-data-types/12-array-manipulation.js
@@ -2,17 +2,16 @@ var sampleArray = [0,0,7];
 
 var incrementLastArrayElement = function(_array) {
     var newArray = [];
-    // Your code should make newArray equal to an array that has the same
-    // values as _array, but the last number has increased by one.
 
-    // For example:
-    // _array = [1, 2, 3];
-    // turns into:
-    // newArray = [1, 2, 4];
+	// Here's one way I did it in a couple of lines
+	newArray = _array;
+	newArray[newArray.length - 1]++;
 
-    // Your code goes in here!
-
-
+	// Here's the solution as explained in the quiz
+	// newArray = _array.slice(0);
+	// var lastNumber = newArray.pop();
+	// newArray.push(lastNumber + 1);
+	
     // Don't delete this line!
     return newArray;
 };
