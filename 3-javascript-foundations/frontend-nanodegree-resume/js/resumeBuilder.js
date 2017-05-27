51c0b4d710284f59876b13f423e0007f0fcc0aae
David Mallard <correspondence@davidmallard.id.au>
2017-05-27 16:14:52 +1000

    Complete the Internationalize Names quiz

--- 3-javascript-foundations/frontend-nanodegree-resume/js/resumeBuilder.js
+++ 3-javascript-foundations/frontend-nanodegree-resume/js/resumeBuilder.js
@@ -261,3 +261,17 @@ displayWork();
 $(document).click(function(loc) {
 	logClicks(loc.pageX, loc.pageY);
 });
+
+// Internationalize Names
+function inName(oldName) {
+	var finalName = oldName;
+
+	var names = finalName.trim().split(' ');
+	names[0] = names[0].slice(0,1).toUpperCase() + names[0].slice(1).toLowerCase();
+	names[1] = names[1].toUpperCase();
+	finalName = names.join(' ');
+
+    return finalName;
+}
+
+$("#main").append(internationalizeButton);
