d61c2eff98cbd46bffb5b33029a36d6a34ed7b78
David Mallard <correspondence@davidmallard.id.au>
2017-05-27 11:18:29 +1000

    Add the initial file for String Manipulation Quiz 1

---
+++ 3-javascript-foundations/09-data-types/06-string-manipulation-quiz-1.js
@@ -0,0 +1,13 @@
+var s = "audacity";
+
+var udacityizer = function(s) {
+    // Right now, the variable s === "audacity"
+    // Manipulate s to make it equal to "Udacity"
+    // Your code goes here!
+
+
+    return s;
+};
+
+// Did your code work? The line below will tell you!
+console.log(udacityizer(s));
