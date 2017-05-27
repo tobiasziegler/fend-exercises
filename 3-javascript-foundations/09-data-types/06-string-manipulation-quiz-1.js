9bd27ba9dd93d62794fdc7773366f14a821485c5
David Mallard <correspondence@davidmallard.id.au>
2017-05-27 11:23:45 +1000

    Complete String Manipulation Quiz 1

--- 3-javascript-foundations/09-data-types/06-string-manipulation-quiz-1.js
+++ 3-javascript-foundations/09-data-types/06-string-manipulation-quiz-1.js
@@ -1,10 +1,7 @@
 var s = "audacity";
 
 var udacityizer = function(s) {
-    // Right now, the variable s === "audacity"
-    // Manipulate s to make it equal to "Udacity"
-    // Your code goes here!
-
+    s = 'U' + s.slice(2);
 
     return s;
 };
