1201857d1dde6467a15d0350126e87275811f5ec
David Mallard <correspondence@davidmallard.id.au>
2017-05-31 10:36:50 +1000

    Add the initial file for the Refactoring To Consolidate Code quiz

---
+++ 4-intermediate-javascript/05-object-decorator-pattern/10-refactoring-consolidate-code.js
@@ -0,0 +1,19 @@
+// Refactor the carlike function so
+// that the move function is no longer
+// defined as a global variable but is
+// contained within the constructor function.
+
+var carlike = function(obj, loc) {
+    obj.loc = loc;
+    obj.move = move;
+    return obj;
+};
+
+var move = function() {
+    this.loc++;
+};
+
+var amy = carlike({}, 1);
+amy.move();
+var ben = carlike({}, 9);
+ben.move();
