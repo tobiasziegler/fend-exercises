956b63f568273d3cb150587e8e38774c6c502ef5
David Mallard <correspondence@davidmallard.id.au>
2017-05-31 11:48:15 +1000

    Add the initial file for the Introduction To Pseudoclassical Subclasses quiz

---
+++ 4-intermediate-javascript/10-pseudoclassical-subclasses/01-intro-pseudoclassical-subclasses.js
@@ -0,0 +1,15 @@
+var Car = function(loc){
+    this.loc = loc;
+};
+
+// Your code goes here!
+
+var zed = new Car(3);
+zed.move();
+
+// These lines have been commented out because Van hasn't been defined in this example
+/*
+var amy = new Van(9);
+amy.move();
+amy.grab();
+*/
