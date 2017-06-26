5f919403eb340e0ad9c4a364b026fa6c693db952
David Mallard <correspondence@davidmallard.id.au>
2017-06-27 09:22:31 +1000

    Separate layout calculations ahead of batch style changes to fix forced synchronous layout issue

--- 5-advanced-interactive-websites/08-styles-layout/10-stop-fsl/js/main.js
+++ 5-advanced-interactive-websites/08-styles-layout/10-stop-fsl/js/main.js
@@ -449,10 +449,13 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
+    var newwidths = [];
     for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
       var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+      newwidths.push((document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px');
+    }
+    for (i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
+      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidths[i];
     }
   }
 
