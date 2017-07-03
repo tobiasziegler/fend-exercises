e1d03176a56cc5066e9deeff2626d3a228bd304c
David Mallard <correspondence@davidmallard.id.au>
2017-07-03 16:22:49 +1000

    Complete the NYT Implementation quiz

--- 5-advanced-interactive-websites/minicourse-ajax-project/js/script.js
+++ 5-advanced-interactive-websites/minicourse-ajax-project/js/script.js
@@ -7,6 +7,10 @@ function loadData() {
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
 
+    // API keys should be stored in a JSON object named config defined in
+    // js/config.js
+    var nytimesKey = config.NYTIMES_API_KEY;
+
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
@@ -21,6 +25,22 @@ function loadData() {
     var streetviewUrl = '//maps.googleapis.com/maps/api/streetview?size=600x400&location=' +
         address;
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+
+    // load New York Times articles
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' +
+        city + '&api-key=' + nytimesKey;
+
+    $.getJSON(nytimesUrl, function(data) {
+        $nytHeaderElem.text('New York Times Articles About ' + city);
+
+        data.response.docs.forEach(function(doc) {
+            var article = '<li class="article"><h4>' +
+                '<a href="' + doc.web_url + '">' +
+                doc.headline.main + '</a></h4>' +
+                '<p>' + doc.snippet + '</p></li>';
+            $('#nytimes-articles').append(article);
+        });
+    });
 
     return false;
 };
