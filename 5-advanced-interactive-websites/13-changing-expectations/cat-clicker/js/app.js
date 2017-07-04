9b5454f99bd6029145d9d33e7468a39bb0535484
David Mallard <correspondence@davidmallard.id.au>
2017-07-04 10:12:45 +1000

    Implement Cat Clicker Duo (Cat Clicker Requirements 2 quiz)

--- 5-advanced-interactive-websites/13-changing-expectations/cat-clicker/js/app.js
+++ 5-advanced-interactive-websites/13-changing-expectations/cat-clicker/js/app.js
@@ -1,16 +1,33 @@
-var clicks = 0;
+var clicks = [0, 0];
+var names = ['Xuxa the Kitten', 'Dramatic Kitten'];
 
-function updateTracker() {
-	$('#click-tracker').text('Number of clicks: ' + clicks);
+function updateTracker(cat) {
+	var tracker;
+	switch(cat) {
+		case 0:
+			tracker = '#click-tracker-1';
+			break;
+		case 1:
+			tracker = '#click-tracker-2';
+			break;
+	}
+	$(tracker).text('Number of clicks for ' + names[cat] + ': ' + clicks[cat]);
 }
 
-// Record a click and update the tracker
-$('#cat').click(function(e) {
-	clicks++;
-	updateTracker();
+// Record a click and update the tracker for cat 1
+$('#cat1').click(function(e) {
+	clicks[0]++;
+	updateTracker(0);
+});
+
+// Record a click and update the tracker for cat 2
+$('#cat2').click(function(e) {
+	clicks[1]++;
+	updateTracker(1);
 });
 
 // Replace the placeholder text with click tracking when page is loaded
 $(document).ready(function(e) {
-	updateTracker();
+	updateTracker(0);
+	updateTracker(1);
 })
