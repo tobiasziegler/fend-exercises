d5aa72a3dfc67c6d9bae522268f9534c85008e13
David Mallard <correspondence@davidmallard.id.au>
2017-07-16 14:30:48 +1000

    Add Cat Levels to Cat Clicker KO

--- 5-advanced-interactive-websites/15-using-organization-library/cat-clicker-ko/js/app.js
+++ 5-advanced-interactive-websites/15-using-organization-library/cat-clicker-ko/js/app.js
@@ -4,6 +4,14 @@ var ViewModel = function() {
 	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
 	this.imgAttribution = ko.observable('https://www.flickr.com/photos/bigtallguy/434164568');
 
+	this.catLevel = ko.computed(function() {
+		if (this.clickCount() < 10) {
+			return 'Newborn';
+		} else {
+			return 'Infant';
+		}
+	}, this);
+
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
 	};
