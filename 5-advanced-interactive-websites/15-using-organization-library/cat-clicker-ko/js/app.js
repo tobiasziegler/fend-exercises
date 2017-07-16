77ca1e9c3aa4bb752a8c6298f245a5197e1c3e5e
David Mallard <correspondence@davidmallard.id.au>
2017-07-16 14:40:55 +1000

    Complete the Show Cats With Control Structures quiz

--- 5-advanced-interactive-websites/15-using-organization-library/cat-clicker-ko/js/app.js
+++ 5-advanced-interactive-websites/15-using-organization-library/cat-clicker-ko/js/app.js
@@ -3,6 +3,12 @@ var ViewModel = function() {
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
 	this.imgAttribution = ko.observable('https://www.flickr.com/photos/bigtallguy/434164568');
+	this.nicknames = ko.observableArray([
+		'Tabtab',
+		'T-Bone',
+		'Mr. T',
+		'Tabitha Tab Tabby Catty Cat'
+	]);
 
 	this.catLevel = ko.computed(function() {
 		if (this.clickCount() < 10) {
