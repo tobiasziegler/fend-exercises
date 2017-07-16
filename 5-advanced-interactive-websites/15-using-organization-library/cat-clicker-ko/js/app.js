79675000c7e81886abd05eb2929cc88afdd7555a
David Mallard <correspondence@davidmallard.id.au>
2017-07-16 15:38:06 +1000

    Complete the Adding More Cats quiz

--- 5-advanced-interactive-websites/15-using-organization-library/cat-clicker-ko/js/app.js
+++ 5-advanced-interactive-websites/15-using-organization-library/cat-clicker-ko/js/app.js
@@ -1,3 +1,41 @@
+var initialCats = [
+	{
+		clickCount: 0,
+		name: 'Tabby',
+		imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+		imgAttribution: 'https://www.flickr.com/photos/bigtallguy/434164568',
+		nicknames: ['Tabtab', 'T-Bone', 'Mr. T', 'Tabitha Tab Tabby Catty Cat']
+	},
+	{
+		clickCount: 0,
+		name: 'Tiger',
+		imgSrc: 'img/4154543904_6e2428c421_z.jpg',
+		imgAttribution: 'https://www.flickr.com/photos/xshamx/4154543904',
+		nicknames: ['Tigger']
+	},
+	{
+		clickCount: 0,
+		name: 'Scaredy',
+		imgSrc: 'img/22252709_010df3379e_z.jpg',
+		imgAttribution: 'https://www.flickr.com/photos/kpjas/22252709',
+		nicknames: ['Casper']
+	},
+	{
+		clickCount: 0,
+		name: 'Shadow',
+		imgSrc: 'img/1413379559_412a540d29_z.jpg',
+		imgAttribution: 'https://www.flickr.com/photos/malfet/1413379559',
+		nicknames: ['Shooby']
+	},
+	{
+		clickCount: 0,
+		name: 'Sleepy',
+		imgSrc: 'img/9648464288_2516b35537_z.jpg',
+		imgAttribution: 'https://www.flickr.com/photos/onesharp/9648464288',
+		nicknames: ['Zzzzz']
+	}
+];
+
 var Cat = function(data) {
 	this.clickCount = ko.observable(data.clickCount);
 	this.name = ko.observable(data.name);
@@ -30,17 +68,21 @@ var Cat = function(data) {
 var ViewModel = function() {
 	var self = this;
 
-	self.currentCat = ko.observable(new Cat({
-		clickCount: 0,
-		name: 'Tabby',
-		imgSrc: 'img/434164568_fea0ad4013_z.jpg',
-		imgAttribution: 'https://www.flickr.com/photos/bigtallguy/434164568',
-		nicknames: ['Tabtab', 'T-Bone', 'Mr. T', 'Tabitha Tab Tabby Catty Cat']
-	}));
+	self.catList = ko.observableArray([]);
+
+	initialCats.forEach(function(catItem) {
+		self.catList.push(new Cat(catItem));
+	});
+
+	self.currentCat = ko.observable(self.catList()[0]);
 
 	self.incrementCounter = function() {
 		self.currentCat().clickCount(self.currentCat().clickCount() + 1);
 	};
+
+	self.changeCat = function(cat) {
+		self.currentCat(cat);
+	}
 };
 
 ko.applyBindings(new ViewModel());
