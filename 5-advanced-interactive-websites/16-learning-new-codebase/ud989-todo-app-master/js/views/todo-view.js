facc709ecc1e0f9e84af1e4e693f7152960ae79c
David Mallard <correspondence@davidmallard.id.au>
2017-07-16 17:07:38 +1000

    Complete the Adding A New Feature quiz

--- 5-advanced-interactive-websites/16-learning-new-codebase/ud989-todo-app-master/js/views/todo-view.js
+++ 5-advanced-interactive-websites/16-learning-new-codebase/ud989-todo-app-master/js/views/todo-view.js
@@ -18,6 +18,7 @@ var app = app || {};
 		// The DOM events specific to an item.
 		events: {
 			'click .toggle': 'toggleCompleted',
+			'click .priority-btn': 'togglePriority',
 			'dblclick label': 'edit',
 			'click .edit-btn': 'edit',
 			'click .destroy': 'clear',
@@ -51,6 +52,7 @@ var app = app || {};
 
 			this.$el.html(this.template(this.model.toJSON()));
 			this.$el.toggleClass('completed', this.model.get('completed'));
+			this.$el.toggleClass('priority', this.model.get('priority'));
 			this.toggleVisible();
 			this.$input = this.$('.edit');
 			return this;
@@ -68,7 +70,12 @@ var app = app || {};
 
 		// Toggle the `"completed"` state of the model.
 		toggleCompleted: function () {
-			this.model.toggle();
+			this.model.toggleCompleted();
+		},
+
+		// Toggle the `"priority"` state of the model.
+		togglePriority: function () {
+			this.model.togglePriority();
 		},
 
 		// Switch this view into `"editing"` mode, displaying the input field.
